6a3be701b102fd696f9ada8ec477868b28797fa6
Derek <legato_5@yahoo.com>
2020-07-26 00:13:16 -0700

    tests passing using arrays. moving on to linked lists

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,22 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        self.storage.append(value)
+        return self.storage
 
     def pop(self):
-        pass
+        if len(self.storage) == 0:
+            return None
+        else:
+            return self.storage.pop()
